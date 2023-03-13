949304aee2930635094a23c1beccbd437c776199
thuccj <thucvodinh2001@gmail.com>
2023-03-13 09:11:28 +0700

    Update SignIn, SignUp

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,25 +1,43 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+
 import {
+//   auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
+//   signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
+
+//   Code của đăng nhập bằng cách chuyển trang
+
+//   useEffect(async () => {
+//     const response = await getRedirectResult(auth);
+//     console.log(response);
+
+//     if (response) {
+//       const userDocref = createUserDocumentFromAuth(response.user);
+//     }
+//   }, []);
+
   const logGoogleUSer = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocref = createUserDocumentFromAuth(user);
   };
 
-  const logGoogleRedirectUSer = async () => {
-    const { user } = await signInWithGoogleRedirect();
-    console.log(user);
-  };
-
   return (
     <div>
       <h1>Sign In Page</h1>
+      {/* Đăng nhập bằng cách hiện popup */}
       <button onClick={logGoogleUSer}>Sign in with Google Popup</button>
-      <button onClick={logGoogleUSer}>Sign in with Google Redirect</button>
+      {/* Đăng nhập bằng chuyển hướng trang 
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>*/}
+      <SignUpForm />
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -27,29 +28,43 @@ googleProvider.setCustomParameters({
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
-export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
   const userDocRef = doc(db, "users", userAuth.uid);
-  //   console.log(userDocRef);
-
   const userSnapshot = await getDoc(userDocRef);
-  //   console.log(userSnapshot);
-  //   console.log(userSnapshot.exists());
 
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
     try {
-      await setDoc(userDocRef, { displayName, email, createdAt });
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
     } catch (error) {
       console.log("error creating the user", error.message);
     }
   }
 
   return userDocRef;
-}; // Xuat ra UID cua nguoi dung google firebase tra ve
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
